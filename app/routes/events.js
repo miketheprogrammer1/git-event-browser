6756621da844125efa41a98911c761ad58aaf2ee
miketheprogrammer1 <miketheprogrammer@gmail.com>
2018-08-03 22:00:07 -0400

    Using parameters in the JSON call, adding a status message from the model, refining the model object

--- app/routes/events.js
+++ app/routes/events.js
@@ -4,17 +4,31 @@ export default Route.extend({
 
   actions: {
     refreshRepository() {
-      alert(this.get('controller').get('repoOwner'));
       this.refresh();
     }
   },
   
   model() {
-    //TODO: Remove this and make a call to the events api
      // The events API is here: https://api.github.com/repos/:owner/:repo/events
      // eg: https://api.github.com/repos/miketheprogrammer1/git-event-browser/events
-    return Ember.$.getJSON('https://api.github.com/repos/miketheprogrammer1/git-event-browser/events').then((data) => {
-       return data;
+    var modelResponse = {
+       "message" : "There is no data to show", 
+       "events" : [] 
+    };
+    
+    if (this.get('controller') == null) {
+      console.log("controller is null");
+      return modelResponse;
+    }
+    var repoOwner = this.get('controller').get('repoOwner');
+    var repoName = this.get('controller').get('repoName');
+    if(repoOwner === null || repoOwner === '' || repoName === null || repoName === '') {
+      return modelResponse;
+    }
+    return Ember.$.getJSON('https://api.github.com/repos/' + repoOwner + '/' + repoName + '/events').then((data) => {
+       modelResponse.events = data;
+       modelResponse.message = "Displaying " + data.length + " github events";
+       return modelResponse;
     });
   }
 
